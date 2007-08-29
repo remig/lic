16e3d50e20523a1eecb7cae8d56daf65a7200f3c
remigagne <remigagne@3289a233-3a86-e3f1-a974-dc4cec0ada7b>
2007-08-29 00:37:15 +0000

    Split the Font, Line, Fill & Box classes off to a separate Drawables module.  Yet another pretty dumb name, but more code out of ever expanding Model module.

--- Model.py
+++ Model.py
@@ -5,6 +5,7 @@ import cairo
 from LDrawColors import *
 from LDrawFileFormat import *
 from GLHelpers import *
+from Drawables import *
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
@@ -175,81 +176,6 @@ class Instructions():
 		f.writelines(lines)
 		f.close()
 	
-class Font():
-	def __init__(self, size, face = "Arial", color = [0, 0, 0], bold = False, italic = False):
-		self.size = size
-		self.face = face
-		self.color = color
-		self.bold = bold
-		self.italic = italic
-
-	def passToCairo(self, context):
-		""" 
-		Set the specified cairo context's current font info to the info stored in this Font. 
-		This overwrites any current cairo font settings - caller is responsible for caching, if needed.
-		"""
-
-		if self.bold:
-			bold = cairo.FONT_WEIGHT_BOLD
-		else:
-			bold = cairo.FONT_WEIGHT_NORMAL
-		
-		if self.italic:
-			italic = cairo.FONT_SLANT_ITALIC
-		else:
-			italic = cairo.FONT_SLANT_NORMAL
-			
-		context.select_font_face(self.face, italic, bold)
-		context.set_font_size(self.size)
-		context.set_source_rgb(*self.color)
-	
-class Line():
-	"""
-	Drawing properties for any given line an instruction book.
-	"""
-
-	# TODO: Define all members and their units in this class
-	def __init__(self, red = 0, green = 0, blue = 0):
-		self.color = [red, green, blue]  # [red, green, blue], 0.0 - 1.0
-		self.thickness = 0
-		self.dash = 0
-
-class Fill():
-# TODO: Define all members and their units in this class
-	def __init__(self):
-		self.color = [0.0, 0.0, 0.0]  # [red, green, blue], 0.0 - 1.0
-		self.pattern = 0
-		self.image = 0
-	
-class Box():
-	"""
-	Represents a border and fill drawn around a PLI / CSI / page,
-	and the position and size info needed to draw the border.
-	"""
-	
-	def __init__(self, x = UNINIT_PROP, y = UNINIT_PROP, width = UNINIT_PROP, height = UNINIT_PROP):
-		self.line = Line(0, 0, 0)
-		self.fill = Fill()
-		
-		# TODO: Convert all of these to relative values (from 0..1, % of overall width | height)
-		self.x = x
-		self.y = y
-		self.width = width
-		self.height = height
-		
-		self.cornerRadius = 0 # Radius for rounded corners. 0 = square
-		self.internalGap = 10  # Distance from inside edge of border to outside edge of contents
-
-	def draw(self, context):
-		# TODO: Remove this check once all is well
-		if (self.x == UNINIT_PROP or self.y == UNINIT_PROP or self.width == UNINIT_PROP or self.height == UNINIT_PROP):
-			print "ERROR: Trying to draw an uninitialized box!!"
-			return
-		
-		context.set_source_rgb(*self.line.color)
-		context.rectangle(self.x, self.y, self.width, self.height)
-		context.stroke()
-
 class Page():
 	"""
 	A single page in an instruction book.
@@ -440,6 +366,9 @@ class CSI():
 		self.box.width, self.box.height, self.centerOffset = params
 		return True
 
+	def dimensionsToString(self):
+		return "s %d %s %d %d %d %d\n" % (self.step.number, self.filename, self.box.width, self.box.height, self.centerOffset.x, self.centerOffset.y)
+	
 	def callPreviousOGLDisplayLists(self, currentBuffers = None):
 		if (self.step.prevStep):
 			self.step.prevStep.csi.callPreviousOGLDisplayLists(currentBuffers)
@@ -457,9 +386,6 @@ class CSI():
 		# If we get here, no better display list was found, so call the default display list
 		glCallList(self.oglDispIDs[0][0])
 
-	def dimensionsToString(self):
-		return "s %d %s %d %d %d %d\n" % (self.step.number, self.filename, self.box.width, self.box.height, self.centerOffset.x, self.centerOffset.y)
-	
 	def initLayout(self, context):
 		pass
 
