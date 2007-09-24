4281c90ea892d7d79e78d08f64c1b15aec7297b9
remigagne <remigagne@3289a233-3a86-e3f1-a974-dc4cec0ada7b>
2007-09-24 04:46:54 +0000

    Make config prettier

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 # TODO: Get/Set these config settings through the actual GUI
-LDrawPath  = "C:\\LDrawParts\\"                     # Path to LDraw.exe and P & PARTS folder
-POVRay = "C:\\Program Files\\POV-Ray\\bin\\pvengine.exe"  # pov executable
-L3P    = "C:\\LDraw\\apps\\l3p\\l3p.exe"            # Path to l3p executable
+
+# Path to LDraw.exe and P & PARTS folder
+LDrawPath = "C:\\LDrawParts"
+
+# Path to l3p executable
+L3P = "C:\\LDraw\\apps\\l3p\\l3p.exe"
+
+# POV-Ray executable
+POVRay = "C:\\Program Files\\POV-Ray\\bin\\pvengine.exe"  
