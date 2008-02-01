33f247dcd96841712f472c0002e33c4d79d90cc0
remigagne <remigagne@3289a233-3a86-e3f1-a974-dc4cec0ada7b>
2008-02-01 02:47:38 +0000

    A few small cleanups to the LicUndoCommand module.

--- LicUndoActions.py
+++ LicUndoActions.py
@@ -1,4 +1,6 @@
-from PyQt4.QtGui import *
+from PyQt4.QtGui import QUndoCommand
+
+QUndoCommand.id = lambda self: self._id
 
 class MoveCommand(QUndoCommand):
 
@@ -7,6 +9,8 @@ class MoveCommand(QUndoCommand):
     itemList[0] = (item, item.oldPos, item.newPos)
     """
 
+    _id = 123
+    
     def __init__(self, itemList):
         QUndoCommand.__init__(self, "Undo the last Page element movement")
 
@@ -14,19 +18,14 @@ class MoveCommand(QUndoCommand):
         for item in itemList:
             self.itemList.append((item, item.oldPos, item.pos()))
 
-    def id(self):
-        return 123
-
     def undo(self):
-        for i in self.itemList:
-            item, oldPos, newPos = i
+        for item, oldPos, newPos in self.itemList:
             item.setPos(oldPos)
             if hasattr(item.parentItem(), "resetRect"):
                 item.parentItem().resetRect()
 
     def redo(self):
-        for i in self.itemList:
-            item, oldPos, newPos = i
+        for item, oldPos, newPos in self.itemList:
             item.setPos(newPos)
             if hasattr(item.parentItem(), "resetRect"):
                 item.parentItem().resetRect()
@@ -38,12 +37,11 @@ class DisplacePartCommand(QUndoCommand):
     partList[0] = (part, oldDisplacement, newDisplacement)
     """
 
+    _id = MoveCommand._id + 1
+
     def __init__(self, partList):
         QUndoCommand.__init__(self, "Undo the last Part displacement")
         self.partList = list(partList)
-
-    def id(self):
-        return 124
 
     def undo(self):
         for part, oldPos, newPos in self.partList:
@@ -62,6 +60,8 @@ class ResizeCSIPLICommand(QUndoCommand):
     sizes = ((oldCSISize, newCSISize), (oldPLISize, newPLISize))
     """
 
+    _id = DisplacePartCommand._id + 1
+
     def __init__(self, instructions, sizes):
         QUndoCommand.__init__(self, "Undo the last CSI | PLI image resize")
         
@@ -70,9 +70,6 @@ class ResizeCSIPLICommand(QUndoCommand):
         self.oldCSISize, self.newCSISize = csiSizes
         self.oldPLISize, self.newPLISize = pliSizes
         
-    def id(self):
-        return 125
-    
     def undo(self):
         self.instructions.setCSIPLISize(self.oldCSISize, self.oldPLISize)
     
@@ -94,19 +91,17 @@ class MoveStepToPageCommand(QUndoCommand):
     stepSet stores a list of (step, oldPage, newPage) tuples:
     stepSet = [(step1, oldPage1, newPage1), (step2, oldPage2, newPage2)]
     """
-    
+
+    _id = ResizeCSIPLICommand._id + 1
+
     def __init__(self, stepSet):
         QUndoCommand.__init__(self, "Undo the last Step-to-Page Move")
-        
         self.stepSet = stepSet
-        
-    def id(self):
-        return 126
-    
+
     def undo(self):
         for step, oldPage, newPage in self.stepSet:
             step.moveToPage(oldPage, relayout = True)
-    
+
     def redo(self):
         for step, oldPage, newPage in self.stepSet:
             step.moveToPage(newPage, relayout = True)
