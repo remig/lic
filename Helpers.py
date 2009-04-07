f5647472b0d4f49fdfea69ad40ac30cfbeaaedd0
remigagne <remigagne@3289a233-3a86-e3f1-a974-dc4cec0ada7b>
2009-04-07 01:53:09 +0000

    Move a bunch of never-touched initialization helper code from Model to Helper; get it out of the way.

--- Helpers.py
+++ Helpers.py
@@ -1,7 +1,8 @@
 from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
 # lambda is bound dynamically to the last variable used, so we can't 
-#  use it in a loop for creating menu actions.  Use this instead.
+# use it in a loop for creating menu actions.  Use this instead.
 # usage: menu.addAction("menu text", makeFunc(self.moveToCallout, callout))
 def makeFunc(func, arg):
     def f(): func(arg)
@@ -20,4 +21,65 @@ def getOppositeDirection(direction):
         return Qt.Key_Right
     if direction == Qt.Key_Right:
         return Qt.Key_Left
-            
+
+def genericMousePressEvent(className):
+    def _tmp(self, event):
+
+        if event.button() == Qt.RightButton:
+            return
+        className.mousePressEvent(self, event)
+        for item in self.scene().selectedItems():
+            item.oldPos = item.pos()
+
+    return _tmp
+    
+def genericMouseReleaseEvent(className):
+    
+    def _tmp(self, event):
+
+        if event.button() == Qt.RightButton:
+            return
+        className.mouseReleaseEvent(self, event)
+        if hasattr(self, 'oldPos') and self.pos() != self.oldPos:
+            self.scene().emit(SIGNAL("itemsMoved"), self.scene().selectedItems())
+
+    return _tmp
+                
+def genericItemParent(self):
+    return self.parentItem()
+
+def genericItemData(self, index):
+    return self.dataText
+
+def genericRow(self):
+    if hasattr(self, '_row'):
+        return self._row
+    if hasattr(self, 'parentItem'):
+        parent = self.parentItem()
+        if hasattr(parent, 'getChildRow'):
+            return parent.getChildRow(self)
+    return 0
+
+QGraphicsRectItem.mousePressEvent = genericMousePressEvent(QAbstractGraphicsShapeItem)
+QGraphicsRectItem.mouseReleaseEvent = genericMouseReleaseEvent(QAbstractGraphicsShapeItem)
+
+QGraphicsRectItem.parent = genericItemParent
+QGraphicsRectItem.data = genericItemData
+QGraphicsRectItem.row = genericRow
+
+QGraphicsSimpleTextItem.mousePressEvent = genericMousePressEvent(QAbstractGraphicsShapeItem)
+QGraphicsSimpleTextItem.mouseReleaseEvent = genericMouseReleaseEvent(QAbstractGraphicsShapeItem)
+
+QGraphicsSimpleTextItem.parent = genericItemParent
+QGraphicsSimpleTextItem.data = genericItemData
+QGraphicsSimpleTextItem.row = genericRow
+
+QGraphicsPixmapItem.mousePressEvent = genericMousePressEvent(QGraphicsItem)
+QGraphicsPixmapItem.mouseReleaseEvent = genericMouseReleaseEvent(QGraphicsItem)
+
+QGraphicsPixmapItem.parent = genericItemParent
+QGraphicsPixmapItem.data = genericItemData
+QGraphicsPixmapItem.row = genericRow
+
+def printRect(rect, text = ""):
+    print text + ", l: %f, r: %f, t: %f, b: %f" % (rect.left(), rect.right(), rect.top(), rect.bottom())
