94f7684f917fc35efb823f46f179a6d46874d5b1
remigagne <remigagne@3289a233-3a86-e3f1-a974-dc4cec0ada7b>
2010-03-18 04:58:28 +0000

    Add the GPL preamble to all source files.  Add COPYING, the generic full text of the GPL.

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,32 @@
+"""
+    Lic - Instruction Book Creation software
+    Copyright (C) 2010 Remi Gagne
+
+    This file (setup.py) is part of Lic.
+
+    Lic is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Lic is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/
+"""
+
 from distutils.core import setup
 import py2exe 
+
+"""
+Create a win32 binary re-distributable package.  This creates a handful of
+folders (build, dist, etc) in the src folder.  Everything in dist can then be 
+zipped & distributed.  In there will be Lic.exe, a tiny executable that 
+simply invokes the (bundled) python interpreter with Lic.py.
+"""
 
 # To build: cd c:\lic\src  c:\python25\python.exe setup.py py2exe
 
